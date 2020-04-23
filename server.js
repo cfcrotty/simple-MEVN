1da24e37fd6963fdde4a94747bf22605539007c5
Cara Crotty <cara@caracrotty.com>
2020-04-23 14:27:20 -0700

    add files for MEVN app

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("tiny"));
+var corsOptions = {
+  origin: "http://localhost:8080"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// app.use(bodyParser.text());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.sequelize.sync();
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+require("./routes/tutorial.routes")(app);
+// simple route
+// app.get("*", (req, res) => {
+//   res.json({ message: "Welcome to bezkoder application." });
+// });
+
+app.get("*", function(req, res) {
+  //res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
