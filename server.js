ecaa8673526fa8f82775d2a5c373251b3d2de9ad
Cara Crotty <cara@caracrotty.com>
2020-04-23 22:47:22 -0700

    fix for heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+  next();
+});
 
 app.use(morgan("tiny"));
 var corsOptions = {
