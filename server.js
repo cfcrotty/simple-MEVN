c1352266815ba54b86254799b5d43c755719ba4d
Cara Crotty <cara@caracrotty.com>
2020-04-23 20:31:43 -0700

    fix heroku error

--- server.js
+++ server.js
@@ -25,16 +25,21 @@ db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
 });
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 require("./routes/tutorial.routes")(app);
 // simple route
-app.get("*", (req, res) => {
-  res.json({ message: "Welcome to MEVN application." });
-});
-
-// app.get("*", function(req, res) {
-//   //res.sendFile(path.join(__dirname, "./client/build/index.html"));
-//   res.sendFile(path.join(__dirname, ".client//public/index.html"));
+// app.get("*", (req, res) => {
+//   res.json({ message: "Welcome to MEVN application." });
 // });
+
+app.get("*", function(req, res) {
+  //res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 4000;
